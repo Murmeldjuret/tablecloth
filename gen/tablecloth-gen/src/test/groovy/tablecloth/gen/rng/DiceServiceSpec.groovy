3df925b82362bdfb49b180d8eeb26cb9a204c7a1
Murmel <renegaderodent@gmail.com>
2017-11-17 23:15:15 +0100

    Test of drop high and drop low in DiceService

--- gen/tablecloth-gen/src/test/groovy/tablecloth/gen/rng/DiceServiceSpec.groovy
+++ gen/tablecloth-gen/src/test/groovy/tablecloth/gen/rng/DiceServiceSpec.groovy
@@ -51,4 +51,32 @@ class DiceServiceSpec extends Specification implements ServiceUnitTest<DiceServi
         then:
         sum.first() == 4
     }
+
+    @Unroll('Testing #nrOfDice dice where drop #dropNLowest low and drop #dropNHighest high for a sum of #expectedSum')
+    def "test drop of N dice rolls"() {
+        given:
+        DiceCommand cmd = new DiceCommand(
+            sides: 6,
+            dropNLowest: dropNLowest,
+            dropNHighest: dropNHighest,
+            nrOfDice: 4
+        )
+
+        when:
+        List<Integer> sum = service.rollDice(cmd)
+
+        then:
+        service.randomService.getInt(_) >>> rollTable
+        sum.first() == expectedSum
+
+        where:
+        rollTable    | dropNLowest | dropNHighest || expectedSum
+        [6, 2, 3, 1] | 1           | 1            || 5
+        [6, 2, 3, 1] | 2           | 0            || 9
+        [6, 2, 3, 1] | 0           | 2            || 3
+        [6, 2, 3, 1] | 2           | 1            || 3
+        [6, 2, 3, 1] | 1           | 2            || 2
+        [6, 2, 3, 1] | 3           | 0            || 6
+        [6, 2, 3, 1] | 0           | 3            || 1
+    }
 }
